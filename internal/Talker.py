b13c5fbb1a9e89326a58918c47735bf20209dbe8
James <jamesgrossett1@gmail.com>
2022-04-11 20:21:47 +1000

    udp comms

--- internal/Talker.py
+++ internal/Talker.py
@@ -1,12 +1,15 @@
+from itertools import filterfalse
 import redis
 import time
 
-HOST = '' #IP address of rasberry pi
+HOST = '10.0.0.100' #IP address of rasberry pi
 PORT = 6379 
 
 class Talker():
     def __init__(self):
         self.r = redis.Redis(HOST, PORT)
+        self.r.set('inventory', 66)
+        self.r.set('error', True)
     
     #Sends data into redis under 'label' with content of value
     def send(self, label, value):
