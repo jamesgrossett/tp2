1b9d72397157b4285573b8c0c5acabe8ed7377dd
James <jamesgrossett1@gmail.com>
2022-05-05 16:19:03 +1000

    main logic + stepper

--- internal/GPIOhandler.py
+++ internal/GPIOhandler.py
@@ -1,12 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 
 # GPIO ports for the 7seg pins - abcdefg
 segments =  (5,6,19,13,26,16,20)
 digits = (8,7)
 #sevens eg delay in seconds
 ssdelay = 1/120
+
+#GPIO ports for IR sensor output
+handsensor = 18
+#hand sensor ground is 6 and power is 4
+
+#GPIO ports for LED outputs
+errorled = 27
+dispensingled = 17
+emptyled = 22
+
+#GPIO ports for ULN2003 stepper motor driver
+stepper1_1 = 2
+stepper1_2 = 3
+stepper1_3 = 4
+stepper1_4 = 14
+stepper1_pins = [stepper1_1, stepper1_2, stepper1_3, stepper1_4]
+
+#stepper motor parameters
+stepper_count = 4096*2
+
+stepper_sequence = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
 
 num = {'clr':[1,1,1,1,1,1,1],
     '0':[0,0,0,0,0,0,1],
@@ -33,7 +57,20 @@ class GPIOHandler():
         for digit in digits:
             GPIO.setup(digit, GPIO.OUT)
             GPIO.output(digit, 1)
-    
+
+        #Setup IR sensors
+        GPIO.setup(handsensor, GPIO.IN)
+
+        #Setup LED outputs
+        GPIO.setup(errorled, GPIO.OUT)
+        GPIO.setup(dispensingled, GPIO.OUT)
+        GPIO.setup(emptyled, GPIO.OUT)
+
+        #Setup stepper motor outputs
+        for pin in stepper1_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, 0)
+
     def clear_seven_seg(self):
         GPIO.output(digits[0], 0)
         GPIO.output(digits[1], 0)
@@ -62,6 +99,32 @@ class GPIOHandler():
             GPIO.output(segments[i], display2[i])
         
         time.sleep(ssdelay)
-        
+    
+    #Reads value currently output by hand sensor
+    def read_hand_sensor(self):
+        return GPIO.input(handsensor)
+    
+    #Updates provided led in display
+    def update_led(self, id, value):
+        if (id == 'error'):
+            GPIO.output(errorled, value)
+        elif (id == "dispensing"):
+            GPIO.output(dispensingled, value)
+        elif (id == 'empty'):
+            GPIO.output(emptyled, value)
+    
+    #Sets all LEDs to off
+    def clear_leds(self):
+        GPIO.output(errorled, 0)
+        GPIO.output(dispensingled, 0)
+        GPIO.output(emptyled, 0)
 
-        
+    def rotate_stepper1(self):
+        Step1Counter = 0
+        while(Step1Counter <= stepper_count):
+            for step in stepper_sequence:
+                for i in range(len(stepper1_pins)):
+                    #print(Step1Counter)
+                    GPIO.output(stepper1_pins[i], step[i])
+                    time.sleep(0.001)
+                    Step1Counter+=1
--- internal/main.py
+++ internal/main.py
@@ -3,11 +3,38 @@ import time
 
 if __name__ == '__main__':
     gpio = GPIOHandler()
-    i = 26
+    state = 'waiting'
+    inventory = 5
+
     while True:
-        if (i > 90):
-            i = 0
-        gpio.clear_seven_seg()
-        gpio.update_seven_seg(i)
-        #i+=1
-        #time.sleep(1/200)
+        #Things done in every state go here 
+        
+        #Waiting for user to place their hand
+        if state == 'waiting':
+            gpio.update_seven_seg(inventory)
+            gpio.clear_leds()
+            if (inventory == 0):
+                state = 'empty'
+            if (gpio.read_hand_sensor()):
+                state = 'dispensing'
+        
+        #Dispensing mask
+        elif state == 'dispensing':
+            gpio.update_led('dispensing', 1)
+            gpio.rotate_stepper1()
+            gpio.update_led('dispensing', 0)
+            inventory-=1
+            state = 'waiting'
+        
+        #No inventory to dispense
+        elif state == 'empty' and inventory == 0:
+            if (gpio.read_hand_sensor):
+                gpio.update_led('empty', 1)
+                time.sleep(3)
+                gpio.update_led('empty', 0)
+
+                #Refresh inventory, go to waiting
+                inventory+=5
+                state = 'waiting'
+
+
