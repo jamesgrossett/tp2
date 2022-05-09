a42bbec4e6da224b109624bfeb3f5352d23f2592
James Grossett <jamesgrossett1@gmail.com>
2022-05-10 01:35:51 +1000

    Work on comms

--- external/UDPTester.py
+++ external/UDPTester.py
@@ -5,8 +5,11 @@ from struct import unpack
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+sock.settimeout(5)
+
 # Bind the socket to the port
-host, port = '10.0.0.52', 65000
+host, port = '10.0.0.52', 65000 #Home address
+#host, port = '10.89.209.82', 65000 #UQ address
 server_address = (host, port)
 
 print(f'Starting UDP server on {host} port {port}')
@@ -14,9 +17,10 @@ sock.bind(server_address)
 
 while True:
     # Wait for message
-    message, address = sock.recvfrom(4096)
-
-    print(f'Received {len(message)} bytes:')
-    x, = unpack('1f', message)
-    print(x)
-
+    try:
+        message, address = sock.recvfrom(4096)
+        print(f'Received {len(message)} bytes:')
+        key, value = unpack('2H', message)
+        print(key, value)
+    except socket.timeout:
+        print('Failed to recieve data (TIMEOUT)')
