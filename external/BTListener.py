7efb48a6cf23e4141b3a4db8b5130e70828384d0
James Grossett <jamesgrossett1@gmail.com>
2022-05-21 22:25:31 +1000

    bt stuff

--- external/BTListener.py
+++ external/BTListener.py
@@ -1,24 +1,38 @@
 import socket
 from struct import pack, unpack
+from tkinter import E
 
 BTaddress = '0c:96:e6:b5:0f:bc' #Laptop bluetooth MAC address
-port = 5
+port = 6
 backlog = 1
 size = 1024
 
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((BTaddress, port))
-s.listen(backlog)
+class BluetoothListener():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        print(f'Initialising Bluetooth host on mac address: {BTaddress} port {port}')
+        self.sock.bind((BTaddress, port))
+        self.sock.listen(backlog)
+        self.sock.settimeout(3)
+        self.BTconnect()
 
-try:
-    client, address = s.accept()
-    while 1:
-        data = client.recv(size)
-        if data:
-            key, value = unpack('2H', data)
-            print(key, value)
-            #client.send()
-except:
-    print("Closing socket")
-    client.close()
-    s.close()
+    def BTconnect(self):
+        try: 
+            self.client, self.address = self.sock.accept()
+            print('Successfully accepted connection')
+        except socket.timeout:
+            print('Connection timed out')
+        except:
+            print('Connection failed')
+
+    def recieve_keyvalue(self):
+        try:
+            data = self.client.recv(size)
+            if data:
+                print(f'Received {len(data)} bytes:')
+                data_decoded = data.decode('UTF-8')
+                key = int(data_decoded[0])
+                value = int(data_decoded[1] + data_decoded[2])
+                return key, value
+        except:
+            print("Failed to recieve data (timeout)")
--- external/UDPListener.py
+++ external/UDPListener.py
@@ -3,14 +3,14 @@ import sys
 from struct import unpack, pack
 
 #UDP server address and port
-#HOST, PORT = '10.0.0.52', 65000 #Home address
-HOST, PORT = '10.89.210.83', 65000 #UQ Address
+HOST, PORT = '10.0.0.52', 65000 #Home address
+#HOST, PORT = '10.89.210.83', 65000 #UQ Address
 
 class Listener():
     def __init__(self):
         # Create a UDP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.settimeout(3) #Set socket to timeout after 3 seconds for socket operations
+        #self.sock.settimeout(3) #Set socket to timeout after 3 seconds for socket operations
         # Bind the socket to the port
         self.address = (HOST, PORT)
         print(f'Starting UDP server on {HOST} port {PORT}')
