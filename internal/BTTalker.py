96568c47d85fcbd44cca9842ae126225de9cbe98
James Grossett <jamesgrossett1@gmail.com>
2022-05-21 11:06:56 +1000

    Bluetooth talker implementation

--- internal/BTTalker.py
+++ internal/BTTalker.py
@@ -1,19 +1,29 @@
-import bluetooth
+import socket
 from struct import pack
+import random
 
 BTaddress = '0c:96:e6:b5:0f:bc' #Laptop bluetooth MAC address
-port = 5
+port = 6
 
+class BluetoothTalker():
+    def __init__(self):
+        #Initialised bluetooth socket connection and connect to host
+        self.s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self.s.connect((BTaddress, port))
 
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((BTaddress, port))
-
-while 1:
-    key = 1
-    value = 2
-    message = pack('2H', key, value)
-    s.send(message, 'UTF-8')
-
-#s.close
+    #Sends a key-value pair to the bluetooth host
+    def send_keyvalue(self, key, value):
+        #Account for value cases where value is <10 (single digit). Ignoring cases where value is >99 because this should not ever occur in reality
+        if len(str(value)) == 1:
+            string_value = '0' + str(value)
+            value = int(string_value)
+        
+        
+        message = format("%s%s" % (key, value))
+        print("Sending: %s" % message)
+        try:
+            self.s.send(bytes(message, 'UTF-8'))
+        except:
+            print('Message failed to send (Host down?)')
 
 
